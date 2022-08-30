eccb839de1614cfa20d80f39fd53658dfdbbbd06
haruki-ishiguru <112426092+haruki-ishiguru@users.noreply.github.com>
2022-08-30 08:38:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Health Diner')
+streamlit.subheader('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spiniach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
