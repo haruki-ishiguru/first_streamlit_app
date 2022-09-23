00247fca859820dc76b21f5e98a779820b28edd7
haruki-ishiguru <112426092+haruki-ishiguru@users.noreply.github.com>
2022-09-23 12:06:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ streamlit.write('The user entered', fruit_choice)
 #streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 
 streamlit.header("The fruit load list contains:")
