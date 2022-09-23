456b6a2c88a14a0fb21727b94c44b127686055fb
haruki-ishiguru <112426092+haruki-ishiguru@users.noreply.github.com>
2022-09-23 12:57:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 # New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
+#streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('View Our Fruit List - Add Your Favorites!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -66,9 +67,10 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
   my_data_rows = get_fruit_load_list()
+  my_cnx
   streamlit.dataframe(my_data_rows)
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
